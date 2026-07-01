663e616a7b29ef586bd7b806264b79de7ef0aec0
AlanYuan16 <100052449+AlanYuan16@users.noreply.github.com>
2026-07-01 15:58:21 -0400

    Add: products-of-array-discluding-self - submission-4

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-4.py
@@ -0,0 +1,9 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # Brute force attempt
+        out = [1] * len(nums)
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if j != i:
+                    out[i] *= nums[j]
+        return out
